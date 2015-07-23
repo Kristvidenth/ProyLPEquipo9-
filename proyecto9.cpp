c17d7154704f8393d1e89b1cdecfeb39f171d570
brayanandres <brayan_borja@outlook.es>
2015-07-23 12:23:43 -0500

    menu
    implementacion del menu

--- proyecto9.cpp
+++ proyecto9.cpp
@@ -2,24 +2,59 @@
 #include <conio>
 #include <stdlib>
 
-void lectura (int d, int h, char V[])
+void pausa();
+
+int main()
 {
-   cout<<"Ingrese su horario e clases: \n";
-	for(int i=0; i<d;i++)
-   {
-       for (int j=0;j<h;j++)
-        cin>> V[i,j];
-     }
+    bool bandera=false;
+    char tecla;
+    do
+    {
+        system("cls");
+        cin.clear();
+        cout << "Menu Principal" << endl;
+        cout << "-----------" << endl << endl;
+        cout << "\t1 .- Ingresar una hora de clases" << endl;
+        cout << "\t2 .- Ver horario de hoy" << endl;
+        cout << "\t3 .- Ver horario de la semana" << endl;
+        cout << "\t4 .- Salir" << endl<< endl;
+        cout << "Elije una opcion: ";
+
+        cin >> tecla;
+
+		switch(tecla)
+		{
+			case '1':
+				system("cls");
+				cout << "Ha elejido el ingreso de una nueva hora.\n";
+				pausa();
+				break;
+			case '2':
+				system("cls");
+				cout << "Ha elejido ver su horario para hoy.\n";
+				pausa();
+				break;
+			case '3':
+				system("cls");
+				cout << "Ha elejido ver su horario de la semana.\n";
+				pausa();
+				break;
+			case '4':
+				bandera=true;
+				//exit(1);
+				break;
+			default:
+				system("cls");
+				cout << "Opcion no valida.\a\n";
+				pausa();
+				break;
+		}
+    }while(bandera!=true);
+    return 0;
+}
+void pausa()
+{
+    cout << "Pulsa una tecla para continuar...";
+    getch();
 }
 
-
-main()
-{
-int b, d, h,V[10];
-cout<<"ingrese los dias laborables: \n";
-cin>>d;
-cout<<"Ingrese las horas al dia que labora: \n";
-cin>>h;
-lectura(d,h);
-getch();
-}
